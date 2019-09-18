dafc6c0730d709ef4afff062ebf0d1c3aa6300ca
Olivér Gál <galoliver05@gmail.com>
2019-09-18 14:07:04 +0200

    Add if __name__ and skills.txt

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
 
+
 def roll_dice():
     dice_result = random.randrange(1, 7)
     print(f"Your roll: {dice_result}")
     return dice_result
+
 
 def mainmenu():  
     mainmenu = open("mainmenu.txt", "r")
@@ -15,4 +17,6 @@ def main():
     start = mainmenu()
     print(start)
 
-main()
+
+if __name__ == "__main__":
+    main()
