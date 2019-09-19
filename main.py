cf26ccfb9651644353769cf1927824525f4201d7
Gabor-Jalsovszky <gabor.jalsovszky@gmail.com>
2019-09-19 17:00:05 +0200

    Delete print_skills and skills.txt

--- main.py
+++ main.py
@@ -22,29 +22,20 @@ def get_basic_skill_values():
     starting_team_spirit = team_spirit_roll
     print(f"\nYour starting Team spirit is {starting_team_spirit}.")
     skillset = {"Programming skills" : 1, "Stamina" : starting_stamina, "Team spirit" : starting_team_spirit}
-    # file = open("skills.txt", "w")
-    # file.write(f"Programming skills: 1\t\tStamina: {starting_stamina}\t\t\tTeam spirit: {starting_team_spirit}\n")
-    # file.close()
     return skillset
 
-def story_turn_page(filename, startline):
+def story_turn_page(filename, startline, skillset):
     file = open(filename, "r")
     lines = file.readlines()
     return lines[startline - 1]
-
-
-def print_skills():
-    skills_open = open("skills.txt", "r")
-    skills = skills_open.read()
-    skills_open.close()
-    return skills
 
 
 def other_options():
     print("To do a dojo excercise, press 1.")
     print("To start working on a project, press 2.")
     print("To try passing the Progbasics PA, press 3.")
-    user_input = input("Enter 1, 2 or 3: ")
+    print("To check your skillset, press 4.")
+    user_input = input("Enter 1, 2, 3 or 4: ")
     return user_input
 
 
@@ -77,7 +68,7 @@ def modifying(number_of_stage, user_answer, skillset):
 def start_chapter(number_of_stage, stagedata_dictonary, skillset):
     startline = (stagedata_dictonary.get(number_of_stage)[0]) 
     print("\n")
-    print(story_turn_page("story.txt", number_of_stage))
+    print(story_turn_page("story.txt", number_of_stage, skillset))
     user_answer = get_user_input()
 
     if user_answer == "1":
@@ -90,16 +81,14 @@ def start_game(skillset):
     stages = {1 : (1, 2),
         2 : (3, 4),
         3: ("startline", "start_of_choice")}
-    story_turn_page("story.txt", 1)
+    story_turn_page("story.txt", 1, skillset)
     for stage_number in stages.keys():
         skillset = start_chapter(stage_number, stages, skillset)
     return skillset
 
 def main():
     player_skillset = get_basic_skill_values()
-    print(player_skillset)
     player_skillset = start_game(player_skillset)
-    print(player_skillset)
 
 if __name__ == "__main__":
     main()
