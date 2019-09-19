430bb72df92f38c0432d498f6cf1239160862429
Olivér Gál <galoliver05@gmail.com>
2019-09-19 22:44:09 +0200

    add clear, sleep

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import random
+import os
+import time
+
+
+clear = lambda: os.system('clear')
+
+
+def show_stats(skillset):
+    for key, value in skillset.items():
+        print(str(key) + ": " + str(value))
 
 
 def roll_dice():
@@ -7,6 +17,7 @@ def roll_dice():
         x = input("Press enter to roll: ")
     dice_result = random.randrange(1, 7)
     print(f"Your roll: {dice_result}")
+    time.sleep(2)
     return dice_result
 
 
@@ -14,14 +25,18 @@ def get_basic_skill_values():
     print("\nYour starting Programming skills is 1. Don't worry, it gets better with practice!\n")
     print("To set your starting Stamina, you have to roll twice. We will add 12 to your result.\n")
     first_roll = roll_dice()
+    clear()
     second_roll = roll_dice()
+    clear()
     starting_stamina = first_roll + second_roll + 12
     print(f"\nYour starting Stamina is {starting_stamina}.\n")
     print("To set your starting Team spirit, you have to roll once.\n")
     team_spirit_roll = roll_dice()
+    clear()
     starting_team_spirit = team_spirit_roll
-    print(f"\nYour starting Team spirit is {starting_team_spirit}.")
     skillset = {"Programming skills" : 1, "Stamina" : starting_stamina, "Team spirit" : starting_team_spirit}
+    show_stats(skillset)
+    time.sleep(5)
     return skillset
 
 
@@ -53,10 +68,14 @@ def modify_statpoint(skill, modifier, skillset):
     if modifier == 1:
         skillset[skill] += 1
         print(f"Nice job! Your {skill} has grown by 1.\n")
+        time.sleep(4)
+        clear()
     elif modifier == -1:
         skillset[skill] -= 1
         print(f"That's not very nice. Your {skill} has decreased by 1.\n")
-    print(skillset)
+        time.sleep(4)
+        clear()
+    show_stats(skillset)
     return skillset
 
 
@@ -77,6 +96,7 @@ def start_chapter(number_of_stage, stagedata_dictonary, skillset):
     print("\n")
     print(story_turn_page("story.txt", number_of_stage, skillset))
     user_answer = get_user_input()
+    time.sleep(1)
 
     if user_answer == "1":
         skillset = modifying(number_of_stage, user_answer, skillset)
@@ -97,6 +117,7 @@ def start_game(skillset):
 
 def main():
     player_skillset = get_basic_skill_values()
+    clear()
     player_skillset = start_game(player_skillset)
 
 
